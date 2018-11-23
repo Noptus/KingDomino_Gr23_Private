b4224f608fdfc3c7dbc6b3ec88017ce05024644d
Noptus <archange618@gmail.com>
2018-11-24 00:02:26 +0100

    Update Partie.java

--- Partie.java
+++ Partie.java
@@ -20,22 +20,33 @@ public class Partie
 	{		
 		this.p = p;
 		this.couleurs = couleurs;
-		
 		ordre = defOrderInit();
 		
 		dominos = new Dominos(12 * p.nbTotal);
-		
-		//on affiche les dominos pour tester
+		// test
 		dominos.print();
 		
 		plateaux = new Plateau[p.nbTotal];
 		for(int i = 0; i <p.nbTotal; i++)
 		{
 			plateaux[i] = new Plateau();
-			
+			System.out.println("");
 			//on affiche le plateau pour tester
 			plateaux[i].print();
 		}
+		
+		
+		// Pour chacune des 12 manches
+        for(int i=1 ; i < 12 ; i++){
+        	
+        	Manche manche = new Manche();
+        	
+        }		
+        
+		
+		
+		
+
 		
 	}
 	
@@ -46,19 +57,9 @@ public class Partie
 	//deroulement de la partie, la ou tout se passera
 	public void jouer()
 	{
-		//TANT QUE LA PARTIE N'EST PAS FINIE
 		
-			//piocher 4 dominos (a faire dans la classe dominos: supprimer les dominos pioches et les retourner ici)
-			
-			//FOR SUR L'ORDRE DE JEU
 		
-				//chaque joueur recupere le domino choisit au tour precedent, le place sur son plateau (a faire dans la classe plateau), puis choisit son domino pour le prochain tour
-			
-			// FIN FOR
-		
-			//on actualise l'ordre de jeu pour le prochain tour
-		
-		//FIN TANT QUE
+        
 	}
 	
 	
@@ -69,7 +70,7 @@ public class Partie
 	private List<Integer> defOrderInit() 
 	{
 		
-		System.out.println("Ordre pour la première manche :");
+		System.out.println("Ordre pour la premiÃ¨re manche :");
 		List<Integer> orderInit = new LinkedList<Integer>();
 		
 		if (p.nbTotal != 2 ) 
@@ -107,25 +108,6 @@ public class Partie
 			return orderInit;
 		}
 	}
+
 	
-	//melange la liste
-	private void shuffleList(List<Integer> a) 
-	{
-        int n = a.size();
-        Random random = new Random();
-        random.nextInt();
-        for (int i = 0; i < n; i++) 
-        {
-            int change = i + random.nextInt(n - i);
-            swap(a, i, change);
-        }
-    }
-	
-	//echange la position de deux elements d'une liste
-	private static void swap(List<Integer> a, int i, int change) 
-	{
-        int helper = a.get(i);
-        a.set(i, a.get(change));
-        a.set(change, helper);
-    }	
 }
